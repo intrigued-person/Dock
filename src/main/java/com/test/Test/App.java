c7aa665c617166020cd2a9fc8eefb8e20be0d065
shakeel.abdul <shakeelahmedcse@gmail.com>
2024-07-29 09:49:27 +0530

    1000th time

---
+++ src/main/java/com/test/Test/App.java
@@ -0,0 +1,18 @@
+package com.test.Test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+	public static int sub(int a, int b) {
+		if (a > b) {
+			return a - b;
+		} else {
+			return 0;
+		}
+
+	}
+
+}
---
+++ src/test/java/com/test/Test/AppTest.java
@@ -0,0 +1,19 @@
+package com.test.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AppTest {
+
+	@Test
+	void testSub() {
+		assertEquals(5, App.sub(10, 5));
+	}
+
+	@Test
+	void testSub1() {
+		assertEquals(0, App.sub(5, 15));
+	}
+
+}
